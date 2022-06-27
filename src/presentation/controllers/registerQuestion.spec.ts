5b71f83ef428af74c5a9ea724d74a3b50ebab466
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-06-27 18:26:09 -0300

    test: ensure RegisterQuestionController return 400 when no answers is provided

--- src/presentation/controllers/registerQuestion.spec.ts
+++ src/presentation/controllers/registerQuestion.spec.ts
@@ -19,4 +19,16 @@ describe('RegisterQuestionController', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: question'))
   })
+  test('Should return 400 with no answers is provided ', () => {
+    const sut = new RegisterQuestionController()
+    const httpRequest = {
+      body: {
+        question: 'any_question'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: answers'))
+  })
 })
