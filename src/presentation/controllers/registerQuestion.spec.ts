0a5e1a9a89a0a79f90abc5d47c0b5b3bb312bdae
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-06-28 18:57:01 -0300

    test: ensure RegisterQuestionController return 200 when all fields provided

--- src/presentation/controllers/registerQuestion.spec.ts
+++ src/presentation/controllers/registerQuestion.spec.ts
@@ -1,10 +1,11 @@
+import { HttpResponse, HttpRequest } from './../protocols/http'
 import { MissingParamError } from './../errors/missing-param-error'
 import { RegisterQuestionController } from './registerQuestion'
 
 describe('RegisterQuestionController', () => {
   test('Should return 400 with no question is provided ', () => {
     const sut = new RegisterQuestionController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         answers: [
           {
@@ -15,21 +16,41 @@ describe('RegisterQuestionController', () => {
         ]
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('question'))
   })
   test('Should return 400 with no answers is provided ', () => {
     const sut = new RegisterQuestionController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         question: 'any_question'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
 
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('answers'))
+  })
+
+  test('Should return 200 with all fields is provided ', () => {
+    const sut = new RegisterQuestionController()
+    const httpRequest: HttpRequest = {
+      body: {
+        question: 'any_question',
+        answers: [
+          {
+            alternative: 1,
+            statement: 'any_statement',
+            isCorrectAnswer: false
+          }
+        ]
+      }
+    }
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({})
   })
 })
