67523cd92ab3188225518c0ca9263750c0ef082d
Fabricio Cardoso <fabricio.cardoso@vizir.com.br>
2022-06-29 17:51:34 -0300

    refactor: add http-helper to remove duplicated code

--- src/presentation/controllers/registerQuestion.ts
+++ src/presentation/controllers/registerQuestion.ts
@@ -1,13 +1,14 @@
+import { badRequest } from '../helpers/http-helper'
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from './../errors/missing-param-error'
 
 export class RegisterQuestionController {
   handle(httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.question) {
-      return { statusCode: 400, body: new MissingParamError('question') }
+      return badRequest(new MissingParamError('question'))
     }
     if (!httpRequest.body.answers) {
-      return { statusCode: 400, body: new MissingParamError('answers') }
+      return badRequest(new MissingParamError('answers'))
     }
     const response: HttpResponse = { statusCode: 200, body: {} }
     return response
